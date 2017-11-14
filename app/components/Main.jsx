ccc9b270adb82d7c40d72c02cabfc05f38e5f74c
Tara Grimm <tararochegrimm@gmail.com>
2017-11-14 12:54:49 -0500

    Reverting master branch back to where it was before react-router added. Created new branch for react-router.

--- app/components/Main.jsx
+++ app/components/Main.jsx
@@ -1,6 +1,4 @@
 import React, { Component } from 'react';
-import { Route, Switch, Redirect } from 'react-router-dom';
-import HelloWorld from '../components/HelloWorld.jsx';
 
 export default class Main extends React.Component {
     constructor(props) {
@@ -10,13 +8,7 @@ export default class Main extends React.Component {
     render() {
         return (
             <div className="main-container">
-                <Switch>
-                    <Route exact path='/' render={(props) => (
-                            <Redirect to="/HelloWorld" />
-                        )} /> 
-                        {/*redirects you to HelloWorld page when you go to / - may not need this if you have a home page or a page that does not have it's own route*/}
-                    <Route exact path='/HelloWorld' component={HelloWorld}/>
-                </Switch>
+                <p> Hello World!</p>
             </div>
         )
     };
--- app/index.js
+++ app/index.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 require('./styles/scss/style.scss');
-import routes from './config/Routes.jsx';
+import Main from './components/Main.jsx';
 
 if (process.env.NODE_ENV !== "production") {
     console.log('Looks like we are in development mode!');
 };
 
-ReactDOM.render(routes, document.getElementById('app'));
+ReactDOM.render(<Main/>, document.getElementById('app'));
